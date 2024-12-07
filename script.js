a9d71c358a1345b983c2d7a354aca67479ed87b7
Jason Ellsworth <jasonells48@icloud.com>
2024-12-07 12:35:43 +0800

    There is still more to do but im happy with how it is and ready to move on.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const grid = document.querySelector('#gird-container');
 const div = document.querySelector('div');
-
+const text = document.querySelector('#text-input');
+const btn = document.querySelector('#grid-input');
 //builds the first 16x16 grid.
 
-function sixTeen(squares) {
+function squareGrid(squares) {
     for (let i = 1; i < squares; i++) {
         grid.append(createGrid());
         for (let i = 0; i < squares; i++) {
@@ -11,15 +12,24 @@ function sixTeen(squares) {
         }
     }
 }
+function getValue() {
+    let value = text.value;
+    return squareGrid(value);
+
+
+}
 
 function createGrid() {
     let gridSquare = document.createElement('div');
     return gridSquare;
 }
 
-sixTeen(16);
+//let squareGrid1 = SquareGrid(16);
+
+btn.addEventListener('click', () => {
+    getValue();
+})
 
 div.addEventListener('mouseover', event => {
     event.target.style.backgroundColor = 'white';
-
 });
